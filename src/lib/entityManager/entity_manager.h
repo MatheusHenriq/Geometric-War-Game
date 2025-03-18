4ecd7b95d82d9e96bd23df1333cde409bc98b663
Matheus Henrique <matheusdeveloper.henrique@gmail.com>
2025-03-17 21:26:40 -0300

    feat: entityManager

---
+++ src/lib/entityManager/entity_manager.h
@@ -0,0 +1,29 @@
+#ifndef ENTITY_MANAGER_H
+#define ENTITY_MANAGER_H
+#include <iostream>
+#include <map>
+#include <vector>
+#include "entity/entity.h"
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec;
+typedef std::map<std::string, EntityVec> EntityMap;
+
+class EntityManager
+{
+    EntityVec m_entities;
+    EntityVec m_toAdd;
+    EntityMap m_entityMap;
+    size_t m_totalEntities = 0;
+
+public:
+    EntityManager();
+    // called at beggining of each frame by game engine
+    // entities added will now be available to use this frame
+    void update();
+    void removeDeadEntities(EntityVec &vec);
+    std::shared_ptr<Entity> addEntity(const std::string &tag);
+    EntityVec &getEntities();
+    EntityVec &getEntities(const std::string &tag);
+};
+
+#endif
